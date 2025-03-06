d9bc59e4d2e5784cfe986db6cfa47a584fa63805
DhumaleArvind <monu182k4@gmail.com>
2025-03-06 14:56:45 +0530

    reset to form added

---
+++ vehicle-service-management/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegistrationForm from "../forms/RegistrationForm.jsx";
+
+
+const Register = () => {
+  return (
+    <div>
+      <br></br>
+      <h2>Register</h2>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default Register;
